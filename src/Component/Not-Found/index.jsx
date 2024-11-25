b614616e7a4fe4dfd7fb2390274508a2f0ad8e0d
Pratish1603 <pratishnagrale123@gmail.com>
2024-11-26 00:34:22 +0530

    login update

--- src/Component/Not-Found/index.jsx
+++ src/Component/Not-Found/index.jsx
@@ -1,13 +1,15 @@
 import './index.css'
-
+import errorImage from '../../assets/error-page-not-found-png-image.png';
 
 const NotFound = ()=>(
 
     <div className='not-found'>
-        <img src="https://png.pngtree.com/png-clipart/20220109/original/pngtree-404-error-page-not-found-png-image_7021505.png" width= "300px"/>
-
+          
+        <img src={errorImage} alt="https://png.pngtree.com/png-clipart/20220109/original/pngtree-404-error-page-not-found-png-image_7021505.png" width= "300px"/>
         <br/>
         <h1>The Page You Are Trying Is Not Found </h1>
+        <br/>
+        <button type="button" class="btn btn-outline-dark">GO BACK</button>
 
     </div>
 )
--- src/Component/login/index.jsx
+++ src/Component/login/index.jsx
@@ -1,6 +1,85 @@
+import { useState } from 'react';
+import Cookies from 'js-cookie';
 import './index.css';
 
-const Login = ()=> <h1>Login Component</h1>
+const Login = ()=> {
+
+    const [allValus,setValuse] = useState ({
+        username : "",
+        password : "",
+        errorMsg : ""
+       
+    })
+
+    const onSubmitUserData = async(e) => {
+
+        e.preventDefault();
+
+        const api = "https://apis.ccbp.in/login";
+
+        const userDetails ={
+            username : allValus.username,
+            password : allValus.password
+        }
+
+        const option ={
+            method : "POST",
+            body : JSON.stringify(userDetails)
+           
+        }
+
+        try{
+            const response = await fetch(api,option);
+
+            const data = await response.json();
+
+            console.log( data.error_msg);
+
+            if(response.ok === true){
+
+                setValuse({...allValus,errorMsg : ""});
+                
+                Cookies.set("jwtToken" , data.jwt_token)
+
+            }
+            else{
+
+                setValuse({...allValus,errorMsg : data.error_msg});
+
+            }
+
+        } catch (error) {
+
+           console.log(error);
+
+        }
+        
+
+
+    }
+
+
+    return(
+        <div className='login-cont'>
+
+            <form className="form-cont" onSubmit={onSubmitUserData}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail">Username</label>
+                    <input onChange={(e)=>{setValuse({...allValus,username : e.target.value})}} type="text" className="form-control border-primary" id="exampleInputEmail" aria-describedby="emailHelp"/>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share yourusername with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input onChange={(e)=>{setValuse({...allValus,password : e.target.value})}} type="Password" className="form-control border-primary" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+            <br />
+            <p className='text-danger'>{allValus.errorMsg}</p>
+
+        </div>
+    )
+}
 
 
 
